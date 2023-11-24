be62c382e1f4a86fcd53bd53ae53601a3fee18f3
AadhavSenthil <149591789+AadhavSenthil@users.noreply.github.com>
2023-11-24 09:54:21 -0600

    Add files via upload
    Completed Code, but don't know why I can't create account and just do any input and output in general

--- Users.py
+++ Users.py
@@ -16,7 +16,7 @@ class User:
 
         self.userID = input("UserID:")
         password = input("Password:")
-        cursor.execute('SELECT Users.Password, Users.UserID FROM ? WHERE UserID=? AND Password=?', (self.tableName,self.userID,password))
+        cursor.execute(f'SELECT Users.Password, Users.UserID FROM {self.tableName} WHERE UserID=? AND Password=?', (self.userID,password))
         row = cursor.fetchone()
         if row:
             print("Login Success")
@@ -31,16 +31,61 @@ class User:
         user = sqlite3.connect(self.database)
         cursor = user.cursor()
         
-        self.userID = " "
-        self.loggedIn = False
+        if self.loggedIn == True:
+            cursor.execute(f"UPDATE {self.tableName} SET UserID = ? WHERE UserID = {self.userID} ")
+            self.userID = " "
+            self.loggedIn = False
+
         return False
 
     def viewAccountInformation(self):
-        print()
+        user = sqlite3.connect(self.database)
+        cursor = user.cursor()
+        if self.loggedIn == True:
+            cursor.execute(f'SELECT * FROM {self.tableName} WHERE UserID=?', (self.userID))
+            row = cursor.fetchone()
+            print("Account Info:")
+            if not row:
+                print("No matching User.")
+            else:
+                print(row)
+
+        else:
+            print("Please Login to see Account Information.")
+
+
+    def createAccount(self):
+        user = sqlite3.connect(self.database)
+        cursor = user.cursor()
+        self.userID = input("UserID: ")
+        email = input("Email: ")
+        password = input("Password: ")
+        firstName = input("First Name: ")
+        lastName = input("Last Name: ")
+        address = input("Address: ")
+        city = input("City: ")
+        state = input("State: ")
+        zip = ("Zip: ")
+        payment = ("Payment: ")
+
+        cursor.execute(f"INSERT INTO {self.tableName} (UserID, Email, Password, FirstName, LastName, Address, City, State, Zip, Payment) Values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (self.userID, email, password, firstName, lastName, address, city, state, zip, payment))
+        user.commit()
+
+    def getLoggedIn(self):
+        return self.loggedIn
+
+    def getUserID(self):
+        return self.userID
+
+
+
+
 
 
 
 def main():
     user1 =User('Users.db', 'Users')
+    
+    user1.createAccount()
 
     user1.login()
