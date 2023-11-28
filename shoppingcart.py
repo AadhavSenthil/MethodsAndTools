c455540db3d24b3d0ca2098629a0d06a1ba79a82
Zeke Pulido <esp139@msstate.edu>
2023-11-27 22:47:09 -0600

    Updated code

--- shoppingcart.py
+++ shoppingcart.py
@@ -1,65 +1,72 @@
+import sqlite3
 
 class Cart:
-    # Constructor
-    def __init__(self):
-        pass
-    
-    #Initial set up
-    def __init__(self, dataBaseName, tableName):
-        self.dataBaseName = dataBaseName
+    def __init__(self, databaseName, tableName):
+        self.databaseName = databaseName
         self.tableName = tableName
 
-    # View cart function
-    def viewCart(self, userID, inventoryDatabase):
-        # gets all of the cart items
-        cart_items = self.getCartItems(userID)
-        print("Books in Cart: ")
+    def view_cart(self, userID, inventoryDatabase):
+        connection = sqlite3.connect(self.databaseName)
+        cursor = connection.cursor()
 
-        # iterates through all of the ISBNs for viewing
-        for isbn in cart_items:
-            bookInfo = inventoryDatabase.getBookInfoByISBN(isbn)
-            print(f"ISBN: {isbn}, Title: {bookInfo['title']}, Price: {bookInfo['price']}")
+        # Retrieve cart items for the given user
+        cursor.execute(f"SELECT ISBN FROM {self.tableName} WHERE UserID = ?", (userID,))
+        cart_items = cursor.fetchall()
 
-    def addToCart(self, userID, ISBN):
-        # Function that has been yet to be created but it will add the ISBN to the cart by userID
-        User.addToCart(userID, ISBN)
+        if not cart_items:
+            print("Cart is empty.")
+        else:
+            print("Books in Cart:")
+            for isbn in cart_items:
+                book_info = inventoryDatabase.getBookInfoByISBN(isbn[0])
+                print(f"ISBN: {isbn[0]}, Title: {book_info['Title']}, Stock: {book_info['Stock']}")
 
-        print(f"Book with ISBN: {ISBN} added to the cart!")
+    def add_to_cart(self, userID, ISBN):
+        connection = sqlite3.connect(self.databaseName)
+        cursor = connection.cursor()
 
-    
-    def removeFromCart(self, userID, ISBN):
-        # No idea what the name is for this yet
-        User.removeFromCart(userID, ISBN)
+        # Check if the item is already in the cart
+        cursor.execute(f"SELECT * FROM {self.tableName} WHERE UserID = ? AND ISBN = ?", (userID, ISBN))
+        existing_item = cursor.fetchone()
 
+        if existing_item:
+            print("Item is already in the cart.")
+        else:
+            # Add the item to the cart
+            cursor.execute(f"INSERT INTO {self.tableName} (UserID, ISBN) VALUES (?, ?)", (userID, ISBN))
+            connection.commit()
+            print(f"Book with ISBN: {ISBN} added to the cart!")
+
+    def remove_from_cart(self, userID, ISBN):
+        connection = sqlite3.connect(self.databaseName)
+        cursor = connection.cursor()
+
+        # Remove the item from the cart
+        cursor.execute(f"DELETE FROM {self.tableName} WHERE UserID = ? AND ISBN = ?", (userID, ISBN))
+        connection.commit()
         print(f"Book with ISBN: {ISBN} removed from the cart!")
 
-    def checkOut(self, userID, inventoryDatabase):
-        # Gets the cart items from userID
-        cart_items = self.getCartItems(userID)
+    def checkout(self, userID, inventoryDatabase):
+        connection = sqlite3.connect(self.databaseName)
+        cursor = connection.cursor()
 
-        # If the cart is empty then this is displayed
+        # Retrieve cart items for the given user
+        cursor.execute(f"SELECT ISBN FROM {self.tableName} WHERE UserID = ?", (userID,))
+        cart_items = cursor.fetchall()
+
         if not cart_items:
             print("Cart is empty. There is nothing to check out.")
+        else:
+            total_price = 0
 
-        # initial price set
-        total_price = 0
+            # Calculate total price and update inventory
+            for isbn in cart_items:
+                book_info = inventoryDatabase.get_book_info_by_isbn(isbn[0])
+                total_price += book_info['Price']
+                inventoryDatabase.decrease_stock(isbn[0])
 
-        # Iterates to add up the price
-        for isbn in cart_items:
-            bookInfo = inventoryDatabase.getBookInfoByISBN(isbn)
-            totalPrice += bookInfo['price']
+            # Clear the cart
+            cursor.execute(f"DELETE FROM {self.tableName} WHERE UserID = ?", (userID,))
+            connection.commit()
 
-        # No idea if this will exist just for concept
-        inventoryDatabase.updateStock(cart_items)
-
-        User.clearCart(userID)
-        # prints checkout notifying user
-        print(f"Checkout successful. Total Price: ${total_price:.2f}")
-
-
-    def getCartItems(self, userID):
-        # returns cart item info
-        return User.getCartItems(userID)
-
-
-    
+            print(f"Checkout successful. Total Price: ${total_price:.2f}")
